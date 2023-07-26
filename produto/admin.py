2b8519bbdde0be9ebf9dfd916dc249d25a38e269
Alex Breno <a_breno@hotmail.com>
2023-07-26 17:51:00 -0300

    Projeto e-commerce - Slug automático e cadastro de produtos

--- produto/admin.py
+++ produto/admin.py
@@ -9,10 +9,24 @@ class VariacaoInLine(admin.TabularInline):
 
 
 class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta',
+                    'get_preco_marketing_formatado',
+                    'get_preco_marketing_promocional_formatado']
     inlines = [
         VariacaoInLine
     ]
 
+
+"""
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produtos_imagem/%Y/%m', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+"""
 
 admin.site.register(models.Produto, ProdutoAdmin)
 admin.site.register(models.Variacao)
--- produto/models.py
+++ produto/models.py
@@ -2,27 +2,42 @@ from pathlib import Path
 
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 from PIL import Image
 
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, verbose_name='Produto')
     descricao_curta = models.TextField(max_length=255)
     descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to='produtos_imagem/%Y/%m', blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField()
-    preco_marketing_promocional = models.FloatField(default=0)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name='preço')
+    preco_marketing_promocional = models.FloatField(
+        default=0, verbose_name='preço promo.')
     tipo = models.CharField(max_length=1,
                             default='V',
-                            choices=(('V', 'Variação'),
+                            choices=(('V', 'Variável'),
                                      ('S', 'Simples'),))
 
     def __str__(self):
         return self.nome
 
+    def get_preco_marketing_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_marketing_formatado.short_description = 'preço'
+
+    def get_preco_marketing_promocional_formatado(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_marketing_promocional_formatado.short_description = 'preço promo.'
+
     def save(self, *args, **kwargs):
+
+        slug = slugify(self.nome)
+        if not self.slug:
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
         if self.imagem:
