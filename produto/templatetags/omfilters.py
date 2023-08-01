12efc6b715ebced001370a75ddb74d14ea04d449
Alex Breno <a_breno@hotmail.com>
2023-08-01 18:25:05 -0300

    Projeto e-commerce: Renderizando o carrinho (Parte 1)

--- produto/templatetags/omfilters.py
+++ produto/templatetags/omfilters.py
@@ -6,3 +6,8 @@ register = Library()
 @register.filter
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+
+@register.filter
+def qtd_item_carrinho(carrinho):
+    return sum([item['quantidade'] for item in carrinho.values()])
