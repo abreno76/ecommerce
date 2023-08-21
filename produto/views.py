a9c02614b2999f2489f3cf9ad2c9c641be9f4ce5
Alex Breno <a_breno@hotmail.com>
2023-08-21 18:45:38 -0300

    Projeto e-commerce - Salvando o pedido (Parte 1)

--- produto/views.py
+++ produto/views.py
@@ -8,6 +8,8 @@ from django.views import View
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from perfil.models import Perfil
+
 from . import models
 
 
@@ -163,6 +165,22 @@ class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'Usuário sem perfil.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+            return redirect('produto:lista')
 
         contexto = {
             'usuario': self.request.user,
